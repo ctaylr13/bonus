bc8b541b03b1d4fd599481890acb08d7725b78b7
Charisse Taylor <ctaylr13@gmail.com>
2026-09-08 07:34:14 -0400

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import wyw from '@wyw-in-js/vite'
+
+export default defineConfig({
+  plugins: [
+    wyw({ include: ['**/*.{ts,tsx}'] }),
+    react(),
+  ],
+})
